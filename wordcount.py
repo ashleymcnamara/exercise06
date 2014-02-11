4a377e12b7b50da5e30926ff0673f3d5fb8b0aeb
Hackbright Student <user@mini-003.(none)>
2014-02-11 12:55:50 -0800

    Sorted dictionary into list of tuples. Writing for loop to place in new dictionary.

--- wordcount.py
+++ wordcount.py
@@ -1,26 +1,31 @@
+from sys import argv
 #Renamed Exercise06 to wordcount.py to match Instructions.
 
 #open our text file
-f = open('twain.txt')
+script, filename = argv
+f = open(filename, "r")
 
-#read the file and store in "wholefile" variable. it's a string!
+#read the file and store in "wholefile" variable
 wholefile = f.read()
+#close the file
+f.close()
+#make all letters lowercase
 wholefile = wholefile.lower()
 
-#replace dashes with whitespace, so we can split the string properly. text uses --
+# replace dashes with whitespace, so we can split the string properly. text uses --
 # between two words. strip() only removes punctuation from beginning and end.
 
 wholefile = wholefile.replace("-", " ") 
 
 #split the string/file at whitespace -- this includes all new lines and tabs.
 #to do this, we use split() with no argument, because that includes ALL whitespace.
-# all whitespace = spaces, tabs, new lines, etc.
 
 split_file = wholefile.split()
 
-#we need to get our words into a dictionary! start by declaring an empty dictionary.
-#we'll call it words
+# put words in dictionary. start by declaring an empty dictionary.
+
 words = {}
+
 #write our for loop to strip words of punctuation at beginning and end, and count
 
 for word in split_file:
@@ -34,11 +39,35 @@ for word in split_file:
     else:
         words[stripped_word] += 1
 
-for key, value in words.iteritems():
-    print "%r, %r" % (key, value)
+#for key, value in words.iteritems():
+    #print key, value
+
 #working on extra credit, sorting by values    
 sorted_values = {}
 
-print sorted(words.values())
-print words.keys()
+# print sorted(words.values())
+#print words.keys()
+list_of_words = [(v, k) for k, v in words.items()]
+list_of_words.sort()
+list_of_words.reverse() #makes largest occurence first
+
+for item in list_of_words:
+    if not sorted_values.get(item[0]):
+        sorted_values[item[0]] = [item[1]]
+ 
+    else:
+       current_value = [item[1]]
+       sorted_values[item[0]] = current_value.append([item[1]])
+print sorted_values       
+
+#     #print item
+
+
+
+
+
+
+# use d.items() to get list of dictionary's items
+# invert the ordering of each item's tuple, from (key, value) into (value, key)
+# sort the list
 
